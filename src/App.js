2649ea3356b4fba603f11ebf02c7304215217bb1
olaide-hok <olaidekareem43@yahoo.com>
2022-09-17 19:46:51 +0100

    Setup Infinite scroll functionality

--- src/App.js
+++ src/App.js
@@ -8,16 +8,21 @@ const searchUrl = `https://api.unsplash.com/search/photos/`
 function App() {
   const [loading, setLoading] = useState(false)
   const [photos, setPhotos] = useState([])
+  const [page, setPage] = useState(1)
+
 
   const fetchImages = async () => {
     setLoading(true)
     let url
-    url = `${mainUrl}${clientID}`
+    const urlPage = `&page=${page}`
+    url = `${mainUrl}${clientID}${urlPage}`
 
     try {
       const response = await fetch(url)
       const data = await response.json()
-      setPhotos(data)
+      setPhotos((oldPhotos) => {
+        return [...oldPhotos, ...data]
+      })
       setLoading(false)
 
     } catch (error) {
@@ -27,7 +32,7 @@ function App() {
 
   useEffect(() => {
     fetchImages()
-  }, [])
+  }, [page])
 
   useEffect(() => {
     const event= window.addEventListener('scroll', () => {
@@ -36,6 +41,7 @@ function App() {
         window.innerHeight + window.scrollY >= document.body.scrollHeight - 2
       ) {
         console.log('the end');
+        setPage((oldPage) => oldPage + 1)
       }
     })
 
